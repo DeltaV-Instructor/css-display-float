11a2423dc59dca5db6bf9b6a9c54acac00c902f6
tektechnologies <info@tektechnologies.com>
2023-03-16 11:38:24 -0500

    function invoking

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 'use strict';
 console.log('app file is connected.');
+
+//function declaration d
+function functionName(){
+  console.log('hello from our function');
+}
+
+
+//we need to invoke or call the function to run the code. 
+functionName();
